f37e292a3f412e86af1b428f4b79ca60952d9b27
chemzqm <chemzqm@gmail.com>
2018-12-17 22:50:08 +0800

    fix(provider): regist same clientId multiple times
    Should works with regist codeAction provider with same clientId multiple
    times.

--- src/provider/codeActionmanager.ts
+++ src/provider/codeActionmanager.ts
@@ -28,13 +28,22 @@ export default class CodeActionManager extends Manager<CodeActionProvider> imple
   ): Promise<Map<string, CodeAction[]> | null> {
     let providers = this.getProviders(document)
     if (!providers.length) return null
+    if (context.only) {
+      let { only } = context
+      providers = providers.filter(p => {
+        if (p.kinds && !p.kinds.some(kind => only.indexOf(kind) != -1)) {
+          return false
+        }
+        return true
+      })
+    }
     let res: Map<string, CodeAction[]> = new Map()
     await Promise.all(providers.map(item => {
       let { provider, clientId } = item
       clientId = clientId || uuid()
       return Promise.resolve(provider.provideCodeActions(document, range, context, token)).then(actions => {
         if (!actions || actions.length == 0) return
-        let codeActions: CodeAction[] = []
+        let codeActions: CodeAction[] = res.get(clientId) || []
         for (let action of actions) {
           if (Command.is(action)) {
             codeActions.push(CodeAction.create(action.title, action))
