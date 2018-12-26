5395020553c048b15e3fe3addbba35d6a1738e51
chemzqm <chemzqm@gmail.com>
2018-12-26 22:33:45 +0800

    fix(test): fix fail on CI

--- src/__tests__/client/changedFiles.test.ts
+++ src/__tests__/client/changedFiles.test.ts
@@ -22,7 +22,7 @@ afterEach(async () => {
 describe('Client integration', () => {
 
   it('should send file change notification', (done) => {
-    if (process.env.NODE_ENV == 'test') return done()
+    if (global.hasOwnProperty('__TEST__')) return done()
     let serverModule = path.join(__dirname, './server/testFileWatcher.js')
     let serverOptions: lsclient.ServerOptions = {
       module: serverModule,
