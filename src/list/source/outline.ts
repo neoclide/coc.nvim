f01279db77f9b9e0ada8571e43446a43540fa926
chemzqm <chemzqm@gmail.com>
2019-02-06 09:41:59 +0800

    feat(outline): use ctags as fallback

--- src/list/source/outline.ts
+++ src/list/source/outline.ts
@@ -1,6 +1,11 @@
-import { DocumentSymbol, Location, SymbolInformation, SymbolKind } from 'vscode-languageserver-types'
+import path from 'path'
+import { DocumentSymbol, Location, Range, SymbolInformation, SymbolKind } from 'vscode-languageserver-types'
+import Uri from 'vscode-uri'
 import languages from '../../languages'
+import Document from '../../model/document'
 import { ListContext, ListItem } from '../../types'
+import { runCommand } from '../../util'
+import { writeFile } from '../../util/fs'
 import workspace from '../../workspace'
 import LocationList from './location'
 const logger = require('../../util/logger')('list-symbols')
@@ -14,33 +19,28 @@ export default class Outline extends LocationList {
     let document = workspace.getDocument(buf.id)
     if (!document) return null
     let symbols = await languages.getDocumentSymbol(document.textDocument)
-    // TODO use ctags when possible
-    if (!symbols) throw new Error('document symbol provider not found for current document')
+    if (!symbols) return await this.loadCtagsSymbols(document)
     if (symbols.length == 0) return []
     let items: ListItem[] = []
     let isSymbols = !symbols[0].hasOwnProperty('location')
     if (isSymbols) {
-      (symbols as DocumentSymbol[]).sort(sortSymbols)
-      for (let s of symbols as DocumentSymbol[]) {
-        let kind = getSymbolKind(s.kind)
-        let location = Location.create(document.uri, s.range)
-        items.push({
-          label: `${s.name} [${kind}]`,
-          filterText: `${s.name}`,
-          location
-        })
-        if (s.children && s.children.length) {
-          for (let item of s.children) {
-            let location = Location.create(document.uri, item.range)
-            let kind = getSymbolKind(item.kind)
-            items.push({
-              label: `${item.name} [${kind}]`,
-              filterText: `${item.name}`,
-              location
-            })
+      function addSymbols(symbols: DocumentSymbol[], level = 0): void {
+        symbols.sort(sortSymbols)
+        for (let s of symbols) {
+          let kind = getSymbolKind(s.kind)
+          if (kind == 'Variable') continue
+          let location = Location.create(document.uri, s.selectionRange)
+          items.push({
+            label: `${' '.repeat(level * 2)}${s.name} [${kind}] ${s.range.start.line + 1}`,
+            filterText: `${s.name}`,
+            location
+          })
+          if (s.children && s.children.length) {
+            addSymbols(s.children, level + 1)
           }
         }
       }
+      addSymbols(symbols as DocumentSymbol[])
     } else {
       (symbols as SymbolInformation[]).sort((a, b) => {
         let sa = a.location.range.start
@@ -51,7 +51,7 @@ export default class Outline extends LocationList {
       for (let s of symbols as SymbolInformation[]) {
         let kind = getSymbolKind(s.kind)
         items.push({
-          label: `${s.name} [${kind}]`,
+          label: `${s.name} [${kind}] ${s.location.range.start.line + 1}`,
           filterText: `${s.name}`,
           location: s.location
         })
@@ -65,9 +65,52 @@ export default class Outline extends LocationList {
     nvim.pauseNotification()
     nvim.command('syntax match CocOutlineName /\\v^\\s*\\S+/ contained containedin=CocOutlineLine', true)
     nvim.command('syntax match CocOutlineKind /\\[\\w\\+\\]/ contained containedin=CocOutlineLine', true)
+    nvim.command('syntax match CocOutlineLine /\\d\\+$/ contained containedin=CocOutlineLine', true)
     nvim.command('highlight default link CocOutlineName Normal', true)
     nvim.command('highlight default link CocOutlineKind Typedef', true)
+    nvim.command('highlight default link CocOutlineLine Comment', true)
     nvim.resumeNotification()
+  }
+
+  public async loadCtagsSymbols(document: Document): Promise<ListItem[]> {
+    let uri = Uri.parse(document.uri)
+    let extname = path.extname(uri.fsPath)
+    let content: string
+    let tempname = await this.nvim.call('tempname')
+    let filepath = `${tempname}.${extname}`
+    let escaped = await this.nvim.call('fnameescape', filepath)
+    await writeFile(escaped, document.getDocumentContent())
+    try {
+      content = await runCommand(`ctags -f - --excmd=number --language-force=${document.filetype} ${escaped}`, process.cwd())
+    } catch (e) {
+      // noop
+    }
+    if (!content.trim().length) {
+      content = await runCommand(`ctags -f - --excmd=number ${escaped}`, process.cwd())
+    }
+    content = content.trim()
+    if (!content) return []
+    let lines = content.split('\n')
+    let items: ListItem[] = []
+    for (let line of lines) {
+      let parts = line.split('\t')
+      let lnum = Number(parts[2].replace(/;"$/, ''))
+      let text = document.getline(lnum - 1)
+      if (!text) continue
+      let idx = text.indexOf(parts[0])
+      let start = idx == -1 ? 0 : idx
+      let range: Range = Range.create(lnum - 1, start, lnum - 1, start + parts[0].length)
+      items.push({
+        label: `${parts[0]} [${parts[3]}] ${lnum}`,
+        filterText: parts[0],
+        location: Location.create(document.uri, range),
+        data: { line: lnum }
+      })
+    }
+    items.sort((a, b) => {
+      return a.data.line - b.data.line
+    })
+    return items
   }
 }
 
@@ -133,11 +176,8 @@ function getSymbolKind(kind: SymbolKind): string {
 function sortSymbols(a: DocumentSymbol, b: DocumentSymbol): number {
   let ra = a.selectionRange
   let rb = b.selectionRange
-  if (ra.start.line < rb.start.line) {
-    return -1
-  }
-  if (ra.start.line > rb.start.line) {
-    return 1
+  if (ra.start.line != rb.start.line) {
+    return ra.start.line - rb.start.line
   }
   return ra.start.character - rb.start.character
 }
