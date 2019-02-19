479892bcc4fb13f08fb3e3c0abe1cf447dfebbef
chemzqm <chemzqm@gmail.com>
2019-02-20 01:26:37 +0800

    chore(index): add more export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import commands from './commands'
 import events from './events'
 import languages from './languages'
 import Document from './model/document'
+import Mru from './model/mru'
 import FileSystemWatcher from './model/fileSystemWatcher'
 import services from './services'
 import sources from './sources'
@@ -10,10 +11,12 @@ import extensions from './extensions'
 import listManager from './list/manager'
 import BasicList from './list/basic'
 import diagnosticManager from './diagnostic/manager'
+import { Neovim, Buffer, Window } from '@chemzqm/neovim'
 
 export * from './types'
 export * from './language-client'
 export * from './provider'
 
+export { Neovim, Buffer, Window, Mru }
 export { workspace, events, services, commands, sources, languages, diagnosticManager, Document, FileSystemWatcher, extensions, listManager, BasicList }
 export { disposeAll } from './util'
