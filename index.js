b84da81a1bcb0b9a74eb133435070c076eab863b
chemzqm <chemzqm@gmail.com>
2019-04-03 13:58:12 +0800

    release 52baad80473aef5988cd0fe36d8a5d54c630999b

--- index.js
+++ index.js
@@ -52525,7 +52525,7 @@ class Plugin extends events_1.EventEmitter {
         let out = await this.nvim.call('execute', ['version']);
         channel.appendLine('vim version: ' + out.trim().split('\n', 2)[0]);
         channel.appendLine('node version: ' + process.version);
-        channel.appendLine('coc.nvim version: ' + workspace_1.default.version + ( true ? '-' + "228a9eb183" : undefined));
+        channel.appendLine('coc.nvim version: ' + workspace_1.default.version + ( true ? '-' + "52baad8047" : undefined));
         channel.appendLine('term: ' + (process.env.TERM_PROGRAM || process.env.TERM));
         channel.appendLine('platform: ' + process.platform);
         channel.appendLine('');
