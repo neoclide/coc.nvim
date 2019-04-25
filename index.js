95cb95bc56b9f398629bc2832fd78a51ac65adb7
chemzqm <chemzqm@gmail.com>
2019-04-25 23:45:52 +0800

    release a28185fe75213356e5ea04735c1206faf3bbb953

--- index.js
+++ index.js
@@ -53539,7 +53539,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "c392bd0fb4" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "a28185fe75" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
@@ -59863,12 +59863,12 @@ class DiagnosticManager {
             return position_1.positionInRange(pos, o.range) == 0;
         });
         if (truncate) {
-            if (this.lastShown && object_1.equals(this.lastShown, diagnostics)) {
+            if (diagnostics.length && this.lastShown && object_1.equals(this.lastShown, diagnostics)) {
                 this.floatFactory.close();
                 return;
             }
-            this.lastShown = diagnostics;
         }
+        this.lastShown = diagnostics;
         if (diagnostics.length == 0) {
             if (useFloat) {
                 this.floatFactory.close();
