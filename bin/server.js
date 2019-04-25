0a3fc9f163a62eee7d35e8459f715dbf57013255
chemzqm <chemzqm@gmail.com>
2019-04-25 23:08:46 +0800

    fix(server): fix version check

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,6 @@
 const semver = require('semver')
 const version = process.version.replace('v', '')
-if (!semver.gt(version, '8.10.0')) {
+if (!semver.gte(version, '8.10.0')) {
   console.error('node >= 8.10.0 required, please upgrade nodejs, or use `let g:coc_node_path = "/path/to/node"` in your vimrc')
   process.exit(1)
 }
