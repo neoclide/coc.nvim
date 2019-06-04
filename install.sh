ca4fda99f501e412c5e4df622593b41bdac2853b
chemzqm <chemzqm@gmail.com>
2019-06-04 10:10:11 +0800

    build(install): not install nodejs and yarn
    BREAK CHANGE: remove install of nodejs and yarn in install.sh

--- install.sh
+++ install.sh
@@ -37,17 +37,6 @@ fetch() {
   fi
 }
 
-install_yarn() {
-  if ! command_exists node && ! command_exists nodejs; then
-    echo "Nodejs not found, installing latest LTS"
-    fetch install-node.now.sh/lts | sh
-  fi
-  if ! command_exists yarn && ! command_exists yarnpkg; then
-    echo "Yarn not found, installing yarn."
-    fetch https://yarnpkg.com/install.sh | sh
-  fi
-}
-
 get_latest_release() {
   fetch "https://api.github.com/repos/neoclide/coc.nvim/releases/latest" |
     grep '"tag_name":' |
@@ -62,7 +51,6 @@ else
 fi
 
 download() {
-  install_yarn
   mkdir -p build
   cd build
   if [ "$tag" = "nightly" ]; then
