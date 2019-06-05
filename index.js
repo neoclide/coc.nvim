4547dee7794e92403705667e50cce8a218b14334
chemzqm <chemzqm@gmail.com>
2019-06-05 08:59:46 +0800

    release afd143e9495e2500b51733d09242f160fd8ac217

--- index.js
+++ index.js
@@ -53840,7 +53840,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "2b723006c6" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "afd143e949" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
@@ -54016,7 +54016,14 @@ class Plugin extends events_1.EventEmitter {
                 case 'runCommand':
                     return await handler.runCommand(...args.slice(1));
                 case 'quickfixes':
-                    return await handler.getQuickfixActions();
+                    let range;
+                    if (args[1]) {
+                        range = {
+                            start: { line: args[1] - 1, character: 0 },
+                            end: { line: args[1], character: 0 }
+                        };
+                    }
+                    return await handler.getQuickfixActions(range);
                 case 'doQuickfix':
                     return await handler.doQuickfix();
                 case 'repeatCommand':
