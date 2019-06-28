24c72595f79859d5a2c1782b268852ce532b8856
chemzqm <chemzqm@gmail.com>
2019-06-28 16:05:34 +0800

    release 5288471ef898751142f4f44c9aeeb179becc35e1

--- build/index.js
+++ build/index.js
@@ -54241,7 +54241,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "1c6fd1f6b9" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "5288471ef8" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
