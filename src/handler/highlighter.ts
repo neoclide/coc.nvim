e5da4648adcceb9ff4b08b71e2cfb77aa68a1764
chemzqm <chemzqm@gmail.com>
2019-07-01 09:11:43 +0800

    fix(highlighter): improve luminance of color

--- src/handler/highlighter.ts
+++ src/handler/highlighter.ts
@@ -152,7 +152,13 @@ function toHexColor(color: Color): { red: number, green: number, blue: number } 
 }
 
 function isDark(color: Color): boolean {
-  let { red, green, blue } = toHexColor(color)
-  let luma = 0.2126 * red + 0.7152 * green + 0.0722 * blue
-  return luma < 40
+  // http://www.w3.org/TR/WCAG20/#relativeluminancedef
+  let rgb = [color.red, color.green, color.blue]
+  let lum = []
+  for (let i = 0; i < rgb.length; i++) {
+    let chan = rgb[i]
+    lum[i] = (chan <= 0.03928) ? chan / 12.92 : Math.pow(((chan + 0.055) / 1.055), 2.4)
+  }
+  let luma = 0.2126 * lum[0] + 0.7152 * lum[1] + 0.0722 * lum[2]
+  return luma <= 0.5
 }
