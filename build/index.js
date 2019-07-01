376d1bda6816cc3cefb1185563f570fc7f39a5ce
chemzqm <chemzqm@gmail.com>
2019-07-01 20:28:28 +0800

    release f9856a2687d5cb5787d638b19f9417720722bf78

--- build/index.js
+++ build/index.js
@@ -54264,7 +54264,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "b669bfbd0a" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "f9856a2687" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
