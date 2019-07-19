ec1856993e13aa9d7f3b77f8dda5fa5ecb99ba82
chemzqm <chemzqm@gmail.com>
2019-07-19 09:09:41 +0800

    fix(completion): check pumvisible on CompleteDone

--- build/index.js
+++ build/index.js
@@ -54416,7 +54416,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "d438eaf5e0" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "46a50885d7" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
@@ -56415,19 +56415,16 @@ class Completion {
         events_1.default.on('CursorMovedI', debounce_1.default(async (bufnr, cursor) => {
             // try trigger completion
             let doc = workspace_1.default.getDocument(bufnr);
-            if (this.isActivted || !doc || cursor[1] == 1)
+            if (this.isActivted || !doc || cursor[1] == 1 || !this.latestInsertChar)
                 return;
             let line = doc.getline(cursor[0] - 1);
             if (!line)
                 return;
-            let { latestInsertChar } = this;
             let pre = string_1.byteSlice(line, 0, cursor[1] - 1);
-            if (!latestInsertChar || !pre.endsWith(latestInsertChar))
-                return;
             if (sources_1.default.shouldTrigger(pre, doc.filetype)) {
                 await this.triggerCompletion(doc, pre, false);
             }
-        }, 20));
+        }, 50));
         workspace_1.default.onDidChangeConfiguration(e => {
             if (e.affectsConfiguration('suggest')) {
                 Object.assign(this.config, this.getCompleteConfig());
@@ -56776,6 +56773,9 @@ class Completion {
     async onCompleteDone(item) {
         let { document } = this;
         if (!this.isActivted || !document || !item.hasOwnProperty('word'))
+            return;
+        let visible = await this.nvim.call('pumvisible');
+        if (visible)
             return;
         let opt = Object.assign({}, this.option);
         let resolvedItem = this.getCompleteItem(item);
