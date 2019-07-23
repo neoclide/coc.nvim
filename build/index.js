979985da220f748c6d1658202375346d71c950ee
chemzqm <chemzqm@gmail.com>
2019-07-24 01:16:08 +0800

    fix(sources): fix triggerCharacters & firstMatch of vim source

--- build/index.js
+++ build/index.js
@@ -54329,7 +54329,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "d9ac1b1568" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "29371b19d7" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
