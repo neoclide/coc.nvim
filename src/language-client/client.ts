1a06280c2c8ebd6ecf3e5c12fe742b123f779e50
Bob Matcuk <bmatcuk@gmail.com>
2019-07-22 20:13:05 -1000

    fix(language-client): mismatch createMessageTransports type

--- src/language-client/client.ts
+++ src/language-client/client.ts
@@ -3782,7 +3782,7 @@ export abstract class BaseLanguageClient {
 
   protected abstract createMessageTransports(
     encoding: string
-  ): Thenable<MessageTransports>
+  ): Promise<MessageTransports | null>
 
   private createConnection(): Thenable<IConnection> {
     let errorHandler = (error: Error, message: Message, count: number) => {
