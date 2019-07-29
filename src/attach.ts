ea80840d5735c3405dded6dbf2d91112ecaa1c1a
cosmin-adrian.popescu@db.com <cosminadrianpopescu@gmail.com>
2019-07-29 20:46:06 +0200

    Patched the URI.file function for cygwin paths

--- src/attach.ts
+++ src/attach.ts
@@ -5,11 +5,26 @@ import events from './events'
 import Plugin from './plugin'
 import semver from 'semver'
 import './util/extensions'
+import { URI } from 'vscode-uri'
 const logger = require('./util/logger')('attach')
 const isTest = process.env.NODE_ENV == 'test'
 
 export default (opts: Attach, requestApi = true): Plugin => {
   const nvim: NeovimClient = attach(opts, log4js.getLogger('node-client'), requestApi)
+  // Overwriding the URI.file function in case of cygwin.
+  nvim.eval('has("win32unix")').then(result => {
+    if (!result) {
+      return;
+    }
+    const old_uri = URI.file;
+    nvim.eval('g:coc_cygqwin_path_prefixes').then(prefixes => {
+      URI.file = function(_path): URI{
+        let path = _path.replace(/\\/g, '/');
+        Object.keys(prefixes).forEach(k => path = path.replace(new RegExp('^' + k, 'gi'), prefixes[k]));
+        return old_uri(path);
+      }
+    })
+  })
   const plugin = new Plugin(nvim)
   let clientReady = false
   let initialized = false
