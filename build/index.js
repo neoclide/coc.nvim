64b3ee0ab446dafd81e6b8afc48000cc9ec70a1f
Heyward Fann <fannheyward@users.noreply.github.com>
2019-08-20 12:01:16 +0800

    fix(implementation): typo (#1096)

--- build/index.js
+++ build/index.js
@@ -54675,7 +54675,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "bc017b02e6" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "c97d41aa1e" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
