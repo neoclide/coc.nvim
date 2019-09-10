a9cb4696fd07a6762d4180538be227b370dd8a98
chemzqm <chemzqm@gmail.com>
2019-09-10 15:47:08 +0800

    chore(doc): recommend [g and ]g for navigate diagnostics
    Closes #1119

--- build/index.js
+++ build/index.js
@@ -32253,7 +32253,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "1777523f6d" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "e8e583c8e0" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
