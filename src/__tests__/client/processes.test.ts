33e8f2af6210c9ca0de46410737d23fa74aab542
Heyward Fann <fannheyward@gmail.com>
2019-10-17 09:41:06 +0800

    fix(tests): fix processes test

--- src/__tests__/client/processes.test.ts
+++ src/__tests__/client/processes.test.ts
@@ -1,12 +1,13 @@
-import { terminate } from '../../util/processes'
 import { spawn } from 'child_process'
+import { terminate } from '../../util/processes'
+import helper from '../helper'
 
 describe('terminate', () => {
-
-  it('should terminate process', () => {
+  it('should terminate process', async () => {
     let cwd = process.cwd()
     let child = spawn('sleep', ['10'], { cwd, detached: true })
     let res = terminate(child, cwd)
+    await helper.wait(60)
     expect(res).toBe(true)
     expect(child.connected).toBe(false)
   })
