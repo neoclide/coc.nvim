8e4b7283eb8ef3ac0324aaa6e8bc6eec08c836f2
Qiming Zhao <chemzqm@gmail.com>
2020-01-07 14:31:12 +0800

    chore(config): fix coc-settings.json

--- build/index.js
+++ build/index.js
@@ -32373,7 +32373,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "e5b05bee4a" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "0c24d1d878" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
