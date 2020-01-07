577bf17c4b3335d0824463587c97a7e29461c27f
Qiming Zhao <chemzqm@gmail.com>
2020-01-07 12:06:53 +0800

    fix(configuration): avoid overwrite of user configuration file

--- build/index.js
+++ build/index.js
@@ -32373,7 +32373,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "15f685206a" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "e5b05bee4a" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
