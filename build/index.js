2c3cf10d541fc74913d1120491b445fa2d3a1b05
Heyward Fann <fannheyward@users.noreply.github.com>
2020-01-09 10:08:17 +0800

    feat(workspace): rm .vim from default rootPatterns (#1450)
    `.vim` folder is always in $HOME, used as default
    pattern will always resolved to rootPath

--- build/index.js
+++ build/index.js
@@ -32373,7 +32373,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "0c24d1d878" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "db5ffd2ff0" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
