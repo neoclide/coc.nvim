9bb31f9d03af2fc52f98ec65020b34efdc496883
Qiming Zhao <chemzqm@gmail.com>
2020-01-15 14:30:33 +0800

    fix(diagnostic): fix possible invalid range

--- build/index.js
+++ build/index.js
@@ -32373,7 +32373,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "db5ffd2ff0" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "6700e7468d" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
