ef1f7aa44e94d19b39a80669e392380bcc74dbd7
Heyward Fann <fannheyward@gmail.com>
2020-02-06 22:58:58 +0800

    fix(tests): COC_DATA_HOME env for tests

--- jest.js
+++ jest.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const os = require('os')
 
 process.on('uncaughtException', function(err) {
   let msg = 'Uncaught exception: ' + err.stack
@@ -6,4 +8,5 @@ process.on('uncaughtException', function(err) {
 
 module.exports = async () => {
   process.env.NODE_ENV = 'test'
+  process.env.COC_DATA_HOME = path.join(os.homedir(), '.config/coc')
 }
