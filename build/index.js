13b0aed12eb4af1847772ab5755f155c86dc2ec6
Qiming Zhao <chemzqm@gmail.com>
2020-02-10 15:55:28 +0800

    feat(plugin): add workspaceSymbols action

--- build/index.js
+++ build/index.js
@@ -32373,7 +32373,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "6700e7468d" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "e49ac2e542" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
