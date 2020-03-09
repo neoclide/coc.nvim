480691869de9aad878adbae8b5d868df284ef973
Qiming Zhao <chemzqm@gmail.com>
2020-03-09 09:30:02 +0800

    feat(extensions): check main file existence

--- build/index.js
+++ build/index.js
@@ -35280,7 +35280,7 @@ class Plugin extends events_1.EventEmitter {
         return false;
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "d5e12d81b2" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "ca3fa9392b" : undefined);
     }
     async showInfo() {
         if (!this.infoChannel) {
