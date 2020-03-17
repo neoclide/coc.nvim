44b556ac6b84d3bcc5daa416e2c62f9d96fe3dd7
Qiming Zhao <chemzqm@gmail.com>
2020-03-17 12:16:40 +0800

    fix(util): make process.chdir empty function in sandbox
    Closes #1427

--- src/util/factory.ts
+++ src/util/factory.ts
@@ -32,7 +32,6 @@ const Module: IModule = require('module')
 const REMOVED_GLOBALS = [
   'reallyExit',
   'abort',
-  'chdir',
   'umask',
   'setuid',
   'setgid',
@@ -138,6 +137,8 @@ function createSandbox(filename: string, logger: Logger): ISandbox {
   REMOVED_GLOBALS.forEach(name => {
     sandbox.process[name] = removedGlobalStub(name)
   })
+  // tslint:disable-next-line: no-empty
+  sandbox.process['chdir'] = () => { }
 
   // read-only umask
   sandbox.process.umask = (mask: number) => {
