d2cb89bb73c5b7f6cd9618fe7c4c30498f871cbd
Sam McCall <sam.mccall@gmail.com>
2020-04-27 11:49:39 +0200

    fix(util): repair and test `@memorize`

--- src/util/decorator.ts
+++ src/util/decorator.ts
@@ -1,11 +1,11 @@
 const logger = require('./logger')('util-decorator')
 
 export function memorize<R extends (...args: any[]) => Promise<R>>(_target: any, key: string, descriptor: any): void {
-  let fn = descriptor.get
+  let fn = descriptor.value
   if (typeof fn !== 'function') return
   let memoKey = '$' + key
 
-  descriptor.get = function(...args): Promise<R> {
+  descriptor.value = function(...args): Promise<R> {
     if (this.hasOwnProperty(memoKey)) return Promise.resolve(this[memoKey])
     return new Promise((resolve, reject): void => { // tslint:disable-line
       Promise.resolve(fn.apply(this, args)).then(res => {
