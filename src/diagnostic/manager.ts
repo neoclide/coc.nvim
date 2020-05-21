05f8f7bd71d256733baa9581ce2c4dbf02d366d9
Qiming Zhao <chemzqm@gmail.com>
2020-05-21 20:40:31 +0800

    fix(diagnostic): make coc_diagnostic_disable work for echoMessage

--- src/diagnostic/manager.ts
+++ src/diagnostic/manager.ts
@@ -445,8 +445,8 @@ export class DiagnosticManager implements Disposable {
     if (!this.enabled) return
     if (this.timer) clearTimeout(this.timer)
     let useFloat = config.messageTarget == 'float'
-    let [bufnr, cursor, filetype, mode] = await this.nvim.eval('[bufnr("%"),coc#util#cursor(),&filetype,mode()]') as [number, [number, number], string, string]
-    if (mode != 'n' || bufnr == this.floatFactory.bufnr) return
+    let [bufnr, cursor, filetype, mode, disabled] = await this.nvim.eval('[bufnr("%"),coc#util#cursor(),&filetype,mode(),get(b:,"coc_diagnostic_disable",0)]') as [number, [number, number], string, string, number]
+    if (mode != 'n' || bufnr == this.floatFactory.bufnr || disabled) return
     let diagnostics = this.getDiagnosticsAt(bufnr, cursor)
     if (diagnostics.length == 0) {
       if (useFloat) {
