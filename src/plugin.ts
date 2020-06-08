2bf7615dfe6b6da43b1d5f09c6c07d5d29a80473
Heyward Fann <fannheyward@gmail.com>
2020-06-08 15:34:03 +0800

    fix(plugin): typo
    closes #2046

--- src/plugin.ts
+++ src/plugin.ts
@@ -166,7 +166,7 @@ export default class Plugin extends EventEmitter {
       channel.appendLine('vim version: ' + first + `${workspace.isVim ? ' ' + workspace.env.version : ''}`)
       channel.appendLine('node version: ' + process.version)
       channel.appendLine('coc.nvim version: ' + this.version)
-      channel.appendLine('coc.nivm directory: ' + path.dirname(__dirname))
+      channel.appendLine('coc.nvim directory: ' + path.dirname(__dirname))
       channel.appendLine('term: ' + (process.env.TERM_PROGRAM || process.env.TERM))
       channel.appendLine('platform: ' + process.platform)
       channel.appendLine('')
