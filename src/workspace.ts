730abee4d6d20024d3b73f0d99b08c1aaee2d915
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2020-08-30 01:33:10 +0900

    <nomodeline> for User autocmds
    so that modelines are not processed when unnecessary.
    See `:help <nomodeline>` for details.
    
    ref #452

--- src/workspace.ts
+++ src/workspace.ts
@@ -719,7 +719,7 @@ export class Workspace implements IWorkspace {
       if (this.env.locationlist) {
         nvim.command('CocList --normal --auto-preview location', true)
       } else {
-        nvim.command('doautocmd User CocLocationsChange', true)
+        nvim.command('doautocmd <nomodeline> User CocLocationsChange', true)
       }
     }
   }
