c8ea4b92acce3b47c8f35c54ddc993b43d9aef7c
Qiming Zhao <chemzqm@gmail.com>
2020-09-20 12:04:32 +0800

    feat(list): use zt for adjust preview range

--- src/list/basic.ts
+++ src/list/basic.ts
@@ -244,7 +244,7 @@ export default abstract class BasicList implements IList, Disposable {
       nvim.call('matchaddpos', [this.hlGroup, [pos]], true)
     }
     if (!exists) nvim.command('setl nobuflisted bufhidden=wipe', true)
-    nvim.command('normal! zz', true)
+    nvim.command('normal! zt', true)
     nvim.call('win_gotoid', [winid], true)
     if (workspace.isVim) nvim.command('redraw', true)
     let [, err] = await nvim.resumeNotification()
@@ -288,7 +288,7 @@ export default abstract class BasicList implements IList, Disposable {
     } else if (filetype) {
       nvim.command(`setf ${filetype}`, true)
     }
-    if (lnum && lnum != 1) nvim.command('normal! zz', true)
+    if (lnum && lnum != 1) nvim.command('normal! zt', true)
     nvim.call('win_gotoid', [winid], true)
     if (workspace.isVim) nvim.command('redraw', true)
     let [, err] = await nvim.resumeNotification()
