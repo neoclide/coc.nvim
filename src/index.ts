7eea9f4200880019ee6e701743fe5099eb872ff7
Qiming Zhao <chemzqm@gmail.com>
2020-11-11 21:18:27 +0800

    refactor(floatBuffer): remove unused floatBuffer module

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import events from './events'
 import languages from './languages'
 import Document from './model/document'
 import Mru from './model/mru'
-import FloatBuffer from './model/floatBuffer'
 import FloatFactory from './model/floatFactory'
 import fetch from './model/fetch'
 import download from './model/download'
@@ -28,6 +27,6 @@ export * from './types'
 export * from './language-client'
 export * from './provider'
 
-export { Neovim, Buffer, Window, Highligher, Mru, Watchman, URI as Uri, Disposable, Event, Emitter, FloatFactory, fetch, download, FloatBuffer, ansiparse }
+export { Neovim, Buffer, Window, Highligher, Mru, Watchman, URI as Uri, Disposable, Event, Emitter, FloatFactory, fetch, download, ansiparse }
 export { workspace, window, snippetManager, events, services, commands, sources, languages, diagnosticManager, Document, FileSystemWatcher, extensions, listManager, BasicList }
 export { disposeAll, concurrent, wait, runCommand, isRunning, executable } from './util'
