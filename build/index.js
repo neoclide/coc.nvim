a61ec12dcc1cfe7e5067502a0331dbbb86760221
Qiming Zhao <chemzqm@gmail.com>
2020-11-17 17:47:36 +0800

    chore(README): update qrcode

--- build/index.js
+++ build/index.js
@@ -24028,7 +24028,7 @@ class Plugin extends events_1.EventEmitter {
         });
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "7078234a8b" : undefined);
+        return workspace_1.default.version + ( true ? '-' + "aa062c751e" : undefined);
     }
     hasAction(method) {
         return this.actions.has(method);
