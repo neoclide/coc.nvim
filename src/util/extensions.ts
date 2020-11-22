a9f08e0d0df62eb9cc9f9c3ea77c94e945e475e9
antonk52 <halloy52@gmail.com>
2020-11-22 21:29:00 +0300

    implement logError function

--- src/util/extensions.ts
+++ src/util/extensions.ts
@@ -1,17 +1,7 @@
 const logger = require('./logger')('extensions')
 
-declare interface Promise<T> {
-  /**
-   * Catches task error and ignores them.
-   */
-  logError(): void
-}
-
-/**
- * Explicitly tells that promise should be run asynchonously.
- */
-Promise.prototype.logError = function <T>(this: Promise<T>): void {
-  this.catch(e => {
-    logger.error(e)
-  })
+export default function logError(promise: Promise<unknown>): void {
+    promise.catch(e => {
+        logger.error(e)
+    })
 }
