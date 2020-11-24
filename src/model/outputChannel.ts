8bb6868beb49a8082802e672658555e50ebf7595
Qiming Zhao <chemzqm@gmail.com>
2020-11-24 15:23:25 +0800

    fix(outputChannel): avoid duplicate lines

--- src/model/outputChannel.ts
+++ src/model/outputChannel.ts
@@ -2,87 +2,66 @@ import { Buffer, Neovim } from '@chemzqm/neovim'
 import { Disposable } from 'vscode-languageserver-protocol'
 import { OutputChannel } from '../types'
 import { disposeAll } from '../util'
-import { Mutex } from '../util/mutex'
 const logger = require('../util/logger')("outpubChannel")
-const MAX_STRING_LENGTH: number = require('buffer').constants.MAX_STRING_LENGTH
 
 export default class BufferChannel implements OutputChannel {
-  private mutex = new Mutex()
   private _disposed = false
-  private _content = ''
-  private disposables: Disposable[] = []
   private _showing = false
+  private lines: string[] = ['']
+  private disposables: Disposable[] = []
   constructor(public name: string, private nvim: Neovim) {
   }
 
   public get content(): string {
-    return this._content
+    return this.lines.join('\n')
   }
 
-  private async _append(value: string): Promise<void> {
-    if (!this.validate()) return
-    let release = await this.mutex.acquire()
-    try {
-      let buf = await this.buffer
-      if (buf) {
-        let line = await this.nvim.call('getbufline', [buf.id, '$'])
-        let lines = value.split('\n')
-        await buf.setLines([line + lines[0], ...lines.slice(1)], {
-          start: -2,
-          end: -1,
-          strictIndexing: false
-        })
-      }
-      release()
-    } catch (e) {
-      release()
+  private _append(value: string): void {
+    let { nvim } = this
+    let idx = this.lines.length - 1
+    let newlines = value.split('\n')
+    let lastline = this.lines[idx] + newlines[0]
+    this.lines[idx] = lastline
+    let append = newlines.slice(1)
+    this.lines = this.lines.concat(append)
+    nvim.pauseNotification()
+    nvim.call('setbufline', [this.bufname, '$', lastline], true)
+    if (append.length) {
+      nvim.call('appendbufline', [this.bufname, '$', append], true)
     }
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    nvim.resumeNotification(false, true)
   }
 
   public append(value: string): void {
-    if (this._content.length + value.length >= MAX_STRING_LENGTH) {
-      this.clear(10)
-    }
-    this._content += value
-    this._append(value).logError()
+    if (!this.validate()) return
+    this._append(value)
   }
 
   public appendLine(value: string): void {
-    if (this._content.length + value.length >= MAX_STRING_LENGTH) {
-      this.clear(10)
-    }
-    this._content += value + '\n'
-    this._append(value + '\n').logError()
+    if (!this.validate()) return
+    this._append(value + '\n')
   }
 
   public clear(keep?: number): void {
     if (!this.validate()) return
-    let latest = []
-    if (keep) latest = this._content.split('\n').slice(-keep)
-    this._content = latest.join('\n')
-    this.buffer.then(buf => {
-      if (buf) {
-        // eslint-disable-next-line @typescript-eslint/no-floating-promises
-        buf.setLines(latest, {
-          start: 0,
-          end: -1,
-          strictIndexing: false
-        })
-      }
-    }).logError()
+    let { nvim } = this
+    this.lines = keep ? this.lines.slice(-keep) : []
+    nvim.pauseNotification()
+    nvim.call('deletebufline', [this.bufname, 1, '$'], true)
+    if (this.lines.length) {
+      nvim.call('appendbufline', [this.bufname, '$', this.lines], true)
+    }
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    nvim.resumeNotification(false, true)
   }
 
   public hide(): void {
-    this.nvim.command(`silent! bd! output:///${this.name}`, true)
+    this.nvim.command(`silent! bd! ${this.bufname}`, true)
   }
 
-  private get buffer(): Promise<Buffer | null> {
-    return new Promise((resolve, reject) => {
-      this.nvim.call('bufnr', [`output:///${this.name}`]).then(res => {
-        if (res == -1) return resolve(null)
-        resolve(this.nvim.createBuffer(res))
-      }, reject)
-    })
+  private get bufname(): string {
+    return `output:///${this.name}`
   }
 
   private async openBuffer(preserveFocus?: boolean): Promise<void> {
@@ -116,7 +95,7 @@ export default class BufferChannel implements OutputChannel {
     if (this._disposed) return
     this._disposed = true
     this.hide()
-    this._content = ''
+    this.lines = []
     disposeAll(this.disposables)
   }
 }
