885d695d5c576a29bc260ff4c8eeff27cb510590
Qiming Zhao <chemzqm@gmail.com>
2020-12-18 18:49:10 +0800

    commit 5a4d0212bf556832201a3019beed67774bb86922
    5a4d0212 chore(package): update next version of typings module

--- build/index.js
+++ build/index.js
@@ -23971,7 +23971,7 @@ class Plugin extends events_1.EventEmitter {
         });
     }
     get version() {
-        return workspace_1.default.version + ( true ? '-' + "92c8322ad8" : 0);
+        return workspace_1.default.version + ( true ? '-' + "5a4d0212bf" : 0);
     }
     hasAction(method) {
         return this.actions.has(method);
