ce448a6945d90609bc5c063577e12b859de0834b
Qiming Zhao <chemzqm@gmail.com>
2020-12-21 18:49:49 +0800

    Release 0.0.80

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ })()
+;
