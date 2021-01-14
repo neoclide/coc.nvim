8b78cdbc320698b23baddff52fcf0e8c7c977d53
Heyward Fann <fannheyward@gmail.com>
2021-01-14 18:52:04 +0800

    feat(language-client): readonly WorkspaceFolder

--- src/language-client/workspaceFolders.ts
+++ src/language-client/workspaceFolders.ts
@@ -19,7 +19,7 @@ function access<T, K extends keyof T>(target: T | undefined, key: K): T[K] | und
   return target[key]
 }
 
-function arrayDiff<T>(left: T[], right: T[]): T[] {
+function arrayDiff<T>(left: ReadonlyArray<T>, right: ReadonlyArray<T>): T[] {
   return left.filter(element => !right.includes(element))
 }
 
@@ -31,7 +31,7 @@ export interface WorkspaceFolderWorkspaceMiddleware {
 export class WorkspaceFoldersFeature implements DynamicFeature<void> {
 
   private _listeners: Map<string, Disposable> = new Map<string, Disposable>()
-  private _initialFolders: WorkspaceFolder[] | undefined
+  private _initialFolders: ReadonlyArray<WorkspaceFolder> | undefined
 
   constructor(private _client: BaseLanguageClient) {
   }
@@ -91,12 +91,12 @@ export class WorkspaceFoldersFeature implements DynamicFeature<void> {
         let result: WorkspaceFolder[] = folders.map(folder => this.asProtocol(folder))
         return result
       }
-      let middleware = client.clientOptions.middleware.workspace
+      const middleware = client.clientOptions.middleware.workspace
       return middleware && middleware.workspaceFolders
         ? middleware.workspaceFolders(token, workspaceFolders)
         : workspaceFolders(token)
     })
-    let value = access(access(access(capabilities, 'workspace'), 'workspaceFolders'), 'changeNotifications')
+    const value = access(access(access(capabilities, 'workspace'), 'workspaceFolders'), 'changeNotifications')
     let id: string | undefined
     if (typeof value === 'string') {
       id = value
@@ -121,7 +121,7 @@ export class WorkspaceFoldersFeature implements DynamicFeature<void> {
     this._client.sendNotification(DidChangeWorkspaceFoldersNotification.type, params)
   }
 
-  protected sendInitialEvent(currentWorkspaceFolders: WorkspaceFolder[] | undefined): void {
+  protected sendInitialEvent(currentWorkspaceFolders: ReadonlyArray<WorkspaceFolder> | undefined): void {
     if (this._initialFolders && currentWorkspaceFolders) {
       const removed: WorkspaceFolder[] = arrayDiff(this._initialFolders, currentWorkspaceFolders)
       const added: WorkspaceFolder[] = arrayDiff(currentWorkspaceFolders, this._initialFolders)
