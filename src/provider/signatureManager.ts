0d681b51d2a4b84a268987c6b0683607ef297024
Noah Bogart <nbogart@batterii.com>
2021-07-21 11:37:13 -0400

    Update triggerCharacter reducer

--- src/provider/signatureManager.ts
+++ src/provider/signatureManager.ts
@@ -7,12 +7,7 @@ import { v4 as uuid } from 'uuid'
 export default class SignatureManager extends Manager<SignatureHelpProvider> implements Disposable {
 
   public register(selector: DocumentSelector, provider: SignatureHelpProvider, triggerCharacters?: string[]): Disposable {
-    let characters = triggerCharacters.reduce((p, c) => {
-      if (/^\s+$/.test(c)) {
-        return p.concat(c)
-      }
-      return p.concat(c.split(/\s*/g).filter(s => s.length > 0))
-    }, [] as string[])
+    let characters = triggerCharacters.reduce((p, c) => p.concat(c.length == 1 ? c : c.split(/\s*/g)), [] as string[])
     let item: ProviderItem<SignatureHelpProvider> = {
       id: uuid(),
       selector,
