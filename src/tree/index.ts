b85e2fb91f07333907a3116d094616d048923331
Qiming Zhao <chemzqm@gmail.com>
2021-07-27 05:12:16 +0800

    refactor(TreeView): refetch TreeItem from provider
    Remove unnecessary checkCollapseState for children

--- src/tree/index.ts
+++ src/tree/index.ts
@@ -25,10 +25,6 @@ export interface TreeViewOptions<T> {
    */
   treeDataProvider: TreeDataProvider<T>
   /**
-   * Enable check children for TreeItemCollapsibleState change, default to true
-   */
-  checkCollapseState?: boolean
-  /**
    * Whether the tree supports multi-select. When the tree supports multi-select and a command is executed from the tree,
    * the first argument to the command is the tree item that the command was executed on and the second argument is an
    * array containing all selected tree items.
