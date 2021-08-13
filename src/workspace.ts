006bfd911ea12eb47712a1e2137b12e4454322b7
Qiming Zhao <chemzqm@gmail.com>
2021-08-14 07:41:02 +0800

    feat(workspace): fetchContent on TextChangedP

--- src/workspace.ts
+++ src/workspace.ts
@@ -1353,6 +1353,7 @@ augroup end`
         let doc = this.getDocument(bufnr)
         if (doc && doc.attached) doc.fetchContent()
       }
+      events.on('TextChangedP', onChange, null, this.disposables)
       events.on('TextChangedI', onChange, null, this.disposables)
       events.on('TextChanged', onChange, null, this.disposables)
     }
