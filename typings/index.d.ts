0b8713836fe59e62eec19bb3df792b83e4925ef2
Weirong Xu <weirongxu.raidou@gmail.com>
2021-09-04 11:00:49 +0800

    feat: add SnippetString

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -7499,6 +7499,83 @@ declare module 'coc.nvim' {
     toString(): string
   }
   /**
+   * A snippet string is a template which allows to insert text
+   * and to control the editor cursor when insertion happens.
+   *
+   * A snippet can define tab stops and placeholders with `$1`, `$2`
+   * and `${3:foo}`. `$0` defines the final tab stop, it defaults to
+   * the end of the snippet. Variables are defined with `$name` and
+   * `${name:default value}`. The full snippet syntax is documented
+   * [here](https://code.visualstudio.com/docs/editor/userdefinedsnippets#_creating-your-own-snippets).
+   */
+  export class SnippetString {
+
+    /**
+     * The snippet string.
+     */
+    value: string;
+
+    constructor(
+      /**
+       * The default snippet string.
+       */
+      value?: string
+    );
+
+    /**
+     * Builder-function that appends the given string to
+     * the {@link SnippetString.value `value`} of this snippet string.
+     *
+     * @param string A value to append 'as given'. The string will be escaped.
+     * @return This snippet string.
+     */
+    appendText(string: string): SnippetString;
+
+    /**
+     * Builder-function that appends a tabstop (`$1`, `$2` etc) to
+     * the {@link SnippetString.value `value`} of this snippet string.
+     *
+     * @param number The number of this tabstop, defaults to an auto-increment
+     * value starting at 1.
+     * @return This snippet string.
+     */
+    appendTabstop(number?: number): SnippetString;
+
+    /**
+     * Builder-function that appends a placeholder (`${1:value}`) to
+     * the {@link SnippetString.value `value`} of this snippet string.
+     *
+     * @param value The value of this placeholder - either a string or a function
+     * with which a nested snippet can be created.
+     * @param number The number of this tabstop, defaults to an auto-increment
+     * value starting at 1.
+     * @return This snippet string.
+     */
+    appendPlaceholder(value: string | ((snippet: SnippetString) => any), number?: number): SnippetString;
+
+    /**
+     * Builder-function that appends a choice (`${1|a,b,c|}`) to
+     * the {@link SnippetString.value `value`} of this snippet string.
+     *
+     * @param values The values for choices - the array of strings
+     * @param number The number of this tabstop, defaults to an auto-increment
+     * value starting at 1.
+     * @return This snippet string.
+     */
+    appendChoice(values: string[], number?: number): SnippetString;
+
+    /**
+     * Builder-function that appends a variable (`${VAR}`) to
+     * the {@link SnippetString.value `value`} of this snippet string.
+     *
+     * @param name The name of the variable - excluding the `$`.
+     * @param defaultValue The default value which is used when the variable name cannot
+     * be resolved - either a string or a function with which a nested snippet can be created.
+     * @return This snippet string.
+     */
+    appendVariable(name: string, defaultValue: string | ((snippet: SnippetString) => any)): SnippetString;
+  }
+  /**
    * Manage snippet sessions.
    */
   export namespace snippetManager {
@@ -7518,7 +7595,7 @@ declare module 'coc.nvim' {
      * @param {Range} range Repalce range, insert to current cursor position when undefined.
      * @returns {Promise<boolean>} true when insert success.
      */
-    export function insertSnippet(snippet: string, select?: boolean, range?: Range): Promise<boolean>
+    export function insertSnippet(snippet: string | SnippetString, select?: boolean, range?: Range): Promise<boolean>
 
     /**
      * Jump to next placeholder, only works when snippet session activated.
