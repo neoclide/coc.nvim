047a702231b6e39322661aeef4ed8ec95c9657d5
Heyward Fann <fannheyward@gmail.com>
2021-09-07 16:31:47 +0800

    feat(model): add document.lineAt function

--- src/model/textdocument.ts
+++ src/model/textdocument.ts
@@ -22,9 +22,9 @@ function computeLineOffsets(text: string, isAtLineStart: boolean, textOffset = 0
  */
 export class TextLine {
 
-	private readonly _line: number
-	private readonly _text: string
-	private readonly _isLastLine: boolean
+  private readonly _line: number
+  private readonly _text: string
+  private readonly _isLastLine: boolean
 
   constructor(line: number, text: string, isLastLine: boolean) {
     this._line = line
@@ -32,50 +32,50 @@ export class TextLine {
     this._isLastLine = isLastLine
   }
 
-	/**
-	 * The zero-based line number.
-	 */
+  /**
+   * The zero-based line number.
+   */
   public get lineNumber(): number {
     return this._line
   }
 
-	/**
-	 * The text of this line without the line separator characters.
-	 */
+  /**
+   * The text of this line without the line separator characters.
+   */
   public get text(): string {
     return this._text
   }
 
-	/**
-	 * The range this line covers without the line separator characters.
-	 */
+  /**
+   * The range this line covers without the line separator characters.
+   */
   public get range(): Range {
     return Range.create(this._line, 0, this._line, this._text.length)
   }
 
-	/**
-	 * The range this line covers with the line separator characters.
-	 */
-	public get rangeIncludingLineBreak(): Range {
+  /**
+   * The range this line covers with the line separator characters.
+   */
+  public get rangeIncludingLineBreak(): Range {
     return this._isLastLine ? this.range : Range.create(this._line, 0, this._line + 1, 0)
-	}
+  }
 
-	/**
-	 * The offset of the first character which is not a whitespace character as defined
-	 * by `/\s/`. **Note** that if a line is all whitespace the length of the line is returned.
-	 */
-	public get firstNonWhitespaceCharacterIndex(): number {
-		// TODO@api, rename to 'leadingWhitespaceLength'
-		return /^(\s*)/.exec(this._text)![1].length
-	}
+  /**
+   * The offset of the first character which is not a whitespace character as defined
+   * by `/\s/`. **Note** that if a line is all whitespace the length of the line is returned.
+   */
+  public get firstNonWhitespaceCharacterIndex(): number {
+    // TODO@api, rename to 'leadingWhitespaceLength'
+    return /^(\s*)/.exec(this._text)![1].length
+  }
 
-	/**
-	 * Whether this line is whitespace only, shorthand
-	 * for {@link TextLine.firstNonWhitespaceCharacterIndex} === {@link TextLine.text TextLine.text.length}.
-	 */
-	public get isEmptyOrWhitespace(): boolean {
-		return this.firstNonWhitespaceCharacterIndex === this._text.length
-	}
+  /**
+   * Whether this line is whitespace only, shorthand
+   * for {@link TextLine.firstNonWhitespaceCharacterIndex} === {@link TextLine.text TextLine.text.length}.
+   */
+  public get isEmptyOrWhitespace(): boolean {
+    return this.firstNonWhitespaceCharacterIndex === this._text.length
+  }
 }
 
 /**
