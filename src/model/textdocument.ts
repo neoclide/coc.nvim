29c4afbc045a192ce6996b8f0d6250fd30f889d4
Heyward Fann <fannheyward@gmail.com>
2021-09-07 16:31:47 +0800

    feat(model): add document.lineAt function

--- src/model/textdocument.ts
+++ src/model/textdocument.ts
@@ -14,6 +14,69 @@ function computeLineOffsets(text: string, isAtLineStart: boolean, textOffset = 0
   }
   return result
 }
+/**
+ * Represents a line of text, such as a line of source code.
+ *
+ * TextLine objects are __immutable__. When a {@link TextDocument document} changes,
+ * previously retrieved lines will not represent the latest state.
+ */
+export class TextLine {
+
+	private readonly _line: number
+	private readonly _text: string
+	private readonly _isLastLine: boolean
+
+  constructor(line: number, text: string, isLastLine: boolean) {
+    this._line = line
+    this._text = text
+    this._isLastLine = isLastLine
+  }
+
+	/**
+	 * The zero-based line number.
+	 */
+  public get lineNumber(): number {
+    return this._line
+  }
+
+	/**
+	 * The text of this line without the line separator characters.
+	 */
+  public get text(): string {
+    return this._text
+  }
+
+	/**
+	 * The range this line covers without the line separator characters.
+	 */
+  public get range(): Range {
+    return Range.create(this._line, 0, this._line, this._text.length)
+  }
+
+	/**
+	 * The range this line covers with the line separator characters.
+	 */
+	public get rangeIncludingLineBreak(): Range {
+    return this._isLastLine ? this.range : Range.create(this._line, 0, this._line + 1, 0)
+	}
+
+	/**
+	 * The offset of the first character which is not a whitespace character as defined
+	 * by `/\s/`. **Note** that if a line is all whitespace the length of the line is returned.
+	 */
+	public get firstNonWhitespaceCharacterIndex(): number {
+		// TODO@api, rename to 'leadingWhitespaceLength'
+		return /^(\s*)/.exec(this._text)![1].length
+	}
+
+	/**
+	 * Whether this line is whitespace only, shorthand
+	 * for {@link TextLine.firstNonWhitespaceCharacterIndex} === {@link TextLine.text TextLine.text.length}.
+	 */
+	public get isEmptyOrWhitespace(): boolean {
+		return this.firstNonWhitespaceCharacterIndex === this._text.length
+	}
+}
 
 /**
  * Text document that created with readonly lines.
@@ -47,6 +110,15 @@ export class LinesTextDocument implements TextDocument {
       return this._content.substring(start, end)
     }
     return this._content
+  }
+
+  public lineAt(lineOrPos: number | Position): TextLine {
+    const line = Position.is(lineOrPos) ? lineOrPos.line : lineOrPos
+		if (typeof line !== 'number' || line < 0 || line >= this.lines.length || Math.floor(line) !== line) {
+      throw new Error('Illegal value for `line`')
+    }
+
+    return new TextLine(line, this.lines[line], line === this.lines.length - 1)
   }
 
   public positionAt(offset: number): Position {
