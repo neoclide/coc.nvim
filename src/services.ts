3e7755086bbee4cc1d83b782f90d8396fc1e811f
kevinhwang91 <kevin.hwang@live.com>
2022-02-18 14:56:06 +0800

    fix(services): respect users' settings of `disabledFeatures`
    It's always ignored now.

--- src/services.ts
+++ src/services.ts
@@ -473,7 +473,7 @@ export function getLanguageServerOptions(id: string, name: string, config: Reado
     })
   }
   // compatiable
-  let disabledFeatures: string[] = []
+  let disabledFeatures: string[] = Array.from(config.disabledFeatures || [])
   for (let key of ['disableWorkspaceFolders', 'disableCompletion', 'disableDiagnostics']) {
     if (config[key] === true) {
       let s = key.slice(7)
