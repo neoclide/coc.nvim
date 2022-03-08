b776bc07e7d0890a12b8d05d7f0ae38949ba274c
Qiming Zhao <chemzqm@gmail.com>
2022-03-08 11:24:54 +0800

    chore(util): remove unnecessary code

--- src/core/contentProvider.ts
+++ src/core/contentProvider.ts
@@ -1,5 +1,4 @@
 import { Neovim } from '@chemzqm/neovim'
-import { fieldOffs } from 'tar'
 import { CancellationTokenSource, Disposable, Emitter, Event } from 'vscode-languageserver-protocol'
 import { URI } from 'vscode-uri'
 import events from '../events'
