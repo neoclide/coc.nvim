e56579d4bc1ef3b97d5130f7f1ffce56125d9e1b
wongxy <xiyao.wong@foxmail.com>
2022-03-13 11:30:45 +0800

    feat(commands): add `editor.action.formatDocument` command

--- src/handler/format.ts
+++ src/handler/format.ts
@@ -1,5 +1,6 @@
 import { Neovim } from '@chemzqm/neovim'
 import { CancellationTokenSource, Position, Range, TextEdit } from 'vscode-languageserver-protocol'
+import commandManager from '../commands'
 import events from '../events'
 import languages from '../languages'
 import Document from '../model/document'
@@ -83,6 +84,17 @@ export default class FormatHandler {
       let pre = info.pre[info.pre.length - 1]
       if (pre) await this.tryFormatOnType(pre, bufnr)
     }))
+
+    handler.addDisposable(commandManager.registerCommand('editor.action.formatDocument', async (bufnr?: number) => {
+      let doc: Document
+      if (bufnr) {
+        doc = workspace.getDocument(bufnr)
+      } else {
+        doc  = (await this.handler.getCurrentState()).doc
+      }
+      await this.documentFormat(doc)
+    }))
+    commandManager.titles.set('editor.action.formatDocument', 'Format Document')
   }
 
   private loadPreferences(e?: ConfigurationChangeEvent): void {
