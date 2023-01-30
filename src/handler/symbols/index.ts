19d88eb1121ec6db01e671b5a73bd00ae94b8687
Marc Deop i Argemí <marc@marcdeop.com>
2023-01-30 22:56:12 +0100

    fix(handler): use proper endline for inner function

--- src/handler/symbols/index.ts
+++ src/handler/symbols/index.ts
@@ -152,8 +152,8 @@ export default class Symbols {
     if (inner && selectRange) {
       let { start, end } = selectRange
       let line = doc.getline(start.line + 1)
-      let endLine = doc.getline(end.line - 1)
-      selectRange = Range.create(start.line + 1, line.match(/^\s*/)[0].length, end.line - 1, endLine.length)
+      let endLine = doc.getline(end.line)
+      selectRange = Range.create(start.line + 1, line.match(/^\s*/)[0].length, end.line, endLine.length)
     }
     if (selectRange) {
       await window.selectRange(selectRange)
