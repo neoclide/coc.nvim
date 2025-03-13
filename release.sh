c72907de9c4436dae18ae9861b76f0f8b7b475ce
Qiming Zhao <chemzqm@gmail.com>
2025-03-13 12:26:33 +0800

    chore(release): run lint and test

--- release.sh
+++ release.sh
@@ -3,22 +3,37 @@
 set -e
 [ "$TRACE" ] && set -x
 
-git config --global user.name "GitHub Actions"
-git config --global user.email "actions@github.com"
-git fetch origin release
 git checkout master
-commitmsg=$(git log head --oneline | head -1)
-mkdir -p .release
-cp -r .github bin lua build autoload plugin history.md Readme.md doc .release
-git checkout release
-cp -r .release/* .
-nvim -c 'helptags doc|q'
+npm run lint:typecheck
+if [ $? -ne 0 ]; then
+  echo "tsc 类型检查未通过"
+  exit 1
+fi
 
-changes=$(git status --porcelain)
-if [ -n "$changes" ]; then
-  git add .
-  git commit -m "$commitmsg"
-  git push origin release
-else
-  echo "No need to commit."
+npm run lint
+if [ $? -ne 0 ]; then
+  echo "eslint 检查未通过"
+  exit 1
+fi
+npm test
+
+if [ $? -eq 0 ]; then
+  git config --global user.name "GitHub Actions"
+  git config --global user.email "actions@github.com"
+  git fetch origin release
+  commitmsg=$(git log head --oneline | head -1)
+  mkdir -p .release
+  cp -r .github bin lua build autoload plugin history.md Readme.md doc .release
+  git checkout release
+  cp -r .release/* .
+  nvim -c 'helptags doc|q'
+
+  changes=$(git status --porcelain)
+  if [ -n "$changes" ]; then
+    git add .
+    git commit -m "$commitmsg"
+    git push origin release
+  else
+    echo "No need to commit."
+  fi
 fi
