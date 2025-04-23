f32791ec5bbf4d6b24438ff5556b7c98e75b0669
Qiming Zhao <chemzqm@gmail.com>
2025-04-23 15:35:08 +0800

    feat(nvim): improve set_lines by lua
    Improve the diff patch of current buffer change.
    - Check if new line insert.
    - Check text change instead of insert only.
    - Support change applied for multiple lines.

---
+++ lua/coc/util.lua
@@ -0,0 +1,43 @@
+
+local M = {}
+
+local unpackFn = unpack
+if unpackFn == nil then
+  unpackFn = table.unpack
+end
+
+M.unpack = unpackFn
+
+function M.sendErrorMsg(msg)
+  vim.defer_fn(function()
+    vim.call('coc#rpc#notify', {'nvim_error_event', {0, 'Lua ' .. _VERSION .. ':'.. msg}})
+  end, 10)
+end
+
+local function errorHandler(err)
+    local traceback = debug.traceback(2)
+    return err .. "\n" .. traceback
+end
+
+-- catch the error and send notification to NodeJS
+function M.call(module, func, args)
+  local m = require(module)
+  local method = m[func]
+  local result = nil
+  if method ~= nil then
+    local ok, err = xpcall(function ()
+      result = method(unpackFn(args))
+    end, errorHandler)
+    if not ok then
+      local msg = 'Error on ' .. module .. '[' .. func .. ']" ' .. err
+      M.sendErrorMsg(msg)
+      error(msg)
+    end
+  else
+    local msg = 'Method "' .. module .. '[' .. func .. ']" not exists'
+    M.sendErrorMsg(msg)
+    error(msg)
+  end
+  return result
+end
+return M
