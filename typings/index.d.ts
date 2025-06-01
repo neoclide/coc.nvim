373afd3a3ee12a793547815ab4e0a0d7e7a14131
Qiming Zhao <chemzqm@gmail.com>
2025-06-02 00:43:34 +0800

    feat(typings): export getFeature for inlineCompletion

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6390,6 +6390,34 @@ declare module 'coc.nvim' {
     provideDiagnostics(document: TextDocument | Uri, previousResultId: string | undefined, token: CancellationToken): ProviderResult<DocumentDiagnosticReport>
     provideWorkspaceDiagnostics?(resultIds: PreviousResultId[], token: CancellationToken, resultReporter: ResultReporter): ProviderResult<WorkspaceDiagnosticReport>
   }
+
+  /**
+   * The inline completion item provider interface defines the contract between extensions and
+   * the inline completion feature.
+   *
+   * Providers are asked for completions either explicitly by a user gesture or implicitly when typing.
+   */
+  export interface InlineCompletionItemProvider {
+
+    /**
+     * Provides inline completion items for the given position and document.
+     * If inline completions are enabled, this method will be called whenever the user stopped typing.
+     * It will also be called when the user explicitly triggers inline completions or explicitly asks for the next or previous inline completion.
+     * In that case, all available inline completions should be returned.
+     * `context.triggerKind` can be used to distinguish between these scenarios.
+     * @param document The document inline completions are requested for.
+     * @param position The position inline completions are requested for.
+     * @param context A context object with additional information.
+     * @param token A cancellation token.
+     * @returns An array of completion items or a thenable that resolves to an array of completion items.
+     */
+    provideInlineCompletionItems(
+      document: TextDocument,
+      position: Position,
+      context: InlineCompletionContext,
+      token: CancellationToken
+    ): ProviderResult<InlineCompletionItem[] | InlineCompletionList>
+  }
   // }}
 
   // Classes {{
@@ -13143,6 +13171,7 @@ declare module 'coc.nvim' {
     getFeature(request: 'textDocument/inlayHint'): DynamicFeature<TextDocumentRegistrationOptions> & TextDocumentProviderFeature<InlayHintsProviderShape>
     getFeature(request: 'textDocument/diagnostic'): DynamicFeature<TextDocumentRegistrationOptions> & TextDocumentProviderFeature<DiagnosticProviderShape> & DiagnosticFeatureShape
     getFeature(request: 'workspace/textDocumentContent'): DynamicFeature<TextDocumentRegistrationOptions> & WorkspaceProviderFeature<TextDocumentContentProviderShape>
+    getFeature(request: 'textDocument/inlineCompletion'): DynamicFeature<TextDocumentRegistrationOptions> & TextDocumentProviderFeature<InlineCompletionItemProvider>
   }
 
   /**
