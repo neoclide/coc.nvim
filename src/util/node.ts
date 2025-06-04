3898ba93da951d34333016f4376b9e2ad8f9e1c0
Qiming Zhao <chemzqm@gmail.com>
2025-06-05 01:19:41 +0800

    fix(util): fix types of minimatch and glob

--- src/util/node.ts
+++ src/util/node.ts
@@ -1,21 +1,21 @@
-import type FS from 'fs'
-import type PATH from 'path'
-import type OS from 'os'
-import type CRYPTO from 'crypto'
-import type CHILD_PROCESS from 'child_process'
-import type READLINE from 'readline'
-import { promisify, inspect } from 'util'
 import type STYLES from 'ansi-styles'
+import type CHILD_PROCESS from 'child_process'
+import type CRYPTO from 'crypto'
 import type DEBOUNCE from 'debounce'
-import type GLOB from 'glob'
-import type MINIMATCH from 'minimatch'
-import type WHICH from 'which'
-import type SEMVER from 'semver'
-import type VM from 'vm'
-import type NET from 'net'
-import type STRIPANSI from 'strip-ansi'
 import type FASTDIFF from 'fast-diff'
+import type FS from 'fs'
+import type * as GLOB from 'glob'
+import type * as Minimatch from 'minimatch'
+import type NET from 'net'
+import type OS from 'os'
+import type PATH from 'path'
+import type READLINE from 'readline'
+import type SEMVER from 'semver'
+import type STRIPANSI from 'strip-ansi'
 import type UNIDECODE from 'unidecode'
+import { inspect, promisify } from 'util'
+import type VM from 'vm'
+import type WHICH from 'which'
 
 export const fs = require('fs') as typeof FS
 export const path = require('path') as typeof PATH
@@ -26,7 +26,7 @@ export const debounce = require('debounce') as typeof DEBOUNCE
 export const readline = require('readline') as typeof READLINE
 export const child_process = require('child_process') as typeof CHILD_PROCESS
 export const glob = require('glob') as typeof GLOB
-export const { minimatch } = require('minimatch') as typeof MINIMATCH
+export const { minimatch } = require('minimatch') as typeof Minimatch
 export const which = require('which') as typeof WHICH
 export const semver = require('semver') as typeof SEMVER
 export const vm = require('vm') as typeof VM
@@ -34,4 +34,5 @@ export const net = require('net') as typeof NET
 export const stripAnsi = require('strip-ansi') as typeof STRIPANSI
 export const fastDiff = require('fast-diff') as typeof FASTDIFF
 export const unidecode = require('unidecode') as typeof UNIDECODE
-export { promisify, inspect }
+export { inspect, promisify }
+
