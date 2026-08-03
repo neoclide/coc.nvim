22127fed66ca62dcff6191886a9b7d7cd74910ef
Qiming Zhao <chemzqm@gmail.com>
2026-08-04 05:58:29 +0800

    test(format): cover ranges formatting without provider

--- src/__tests__/handler/format.test.ts
+++ src/__tests__/handler/format.test.ts
@@ -151,6 +151,7 @@ describe('format handler', () => {
       let options = await workspace.getFormatOptions()
       let token = (new CancellationTokenSource()).token
       expect(await languages.provideDocumentRangeFormattingEdits(doc, range, options, token)).toBe(null)
+      expect(await languages.provideDocumentRangesFormattingEdits(doc, [range], options, token)).toBe(null)
       expect(languages.hasProvider(ProviderName.FormatOnType, doc)).toBe(false)
       expect(languages.hasProvider(ProviderName.OnTypeEdit, doc)).toBe(false)
       let edits = await languages.provideDocumentFormattingEdits(doc, options, token)
